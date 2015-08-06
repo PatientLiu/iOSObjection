ff44dafc2a1b420d290e2b92d84a051a3fe5f675
Patient <654444897@qq.com>
2015-08-06 10:01:38 +0800

    dasdasdas

---
+++ HoolayBlock/BlockPatient.h
@@ -0,0 +1,13 @@
+//
+//  BlockPatient.h
+//  HoolayBlock
+//
+//  Created by 刘璞峰 on 15/8/5.
+//  Copyright (c) 2015年 patient. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol BlockPatient <NSObject>
+
+@end
---
+++ HoolayBlock/blockpatientforhoolay.m
@@ -0,0 +1,13 @@
+//
+//  blockpatientforhoolay.m
+//  HoolayBlock
+//
+//  Created by 刘璞峰 on 15/8/5.
+//  Copyright (c) 2015年 patient. All rights reserved.
+//
+
+#import "blockpatientforhoolay.h"
+
+@implementation blockpatientforhoolay
+
+@end
